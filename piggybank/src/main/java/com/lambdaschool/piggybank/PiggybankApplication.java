a57cac031bcd26896702888e94b97b6b7e2022d0
james-coulter <95coulter@gmail.com>
2020-06-05 09:16:21 -0600

    First commit. Set up files

---
+++ piggybank/src/main/java/com/lambdaschool/piggybank/PiggybankApplication.java
@@ -0,0 +1,16 @@
+package com.lambdaschool.piggybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggybankApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("It's Compiling!!");
+
+        SpringApplication.run(PiggybankApplication.class, args);
+    }
+
+}
